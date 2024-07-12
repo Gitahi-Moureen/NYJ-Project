ce8e392c3982148779f8fa924fb4f3f1b7756ddf
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-07-12 20:26:49 +0300

    add changes

--- src/Videos/index.js
+++ src/Videos/index.js
@@ -17,12 +17,12 @@ const Videos = ()=>{
 </div>   
 
 <div><iframe src="https://www.loom.com/embed/c9a0867f680148cb96476650bb0948ff?sid=584fca13-1928-43cb-9466-1bd22077e4a9" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
-                <p className="text"><b>Episode 2:</b> Architecture outline of chatGPT</p>
+                <p className="text"><b>Episode 2:</b> Architecture outline of ChatGPT</p>
                
 </div>
           
 <div ><iframe src="https://www.loom.com/embed/a456880b8c98406db4da1612a303dcd2?sid=bea2c28f-5b66-4b68-988c-3e7f4a586e5b" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
-                <p className="text"><b>Episode 3:</b> Features of chatGPT and<br></br> how the product generates revenue.
+                <p className="text"><b>Episode 3:</b> Features of ChatGPT and<br></br> how the product generates revenue.
 
 </p>
                    
@@ -30,7 +30,7 @@ const Videos = ()=>{
 </div>
 <div className="vidLine2">
 <div><iframe src="https://www.loom.com/embed/8299ce6892aa4416886aea8ab1467720?sid=6ad43d45-2767-44c5-8034-0c8dd936cc82" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
-                <p className="text"><b>Episode 4: </b>Positive impacts and shortcomings <br></br>of chatGPT and the User experience.
+                <p className="text"><b>Episode 4: </b>Positive impacts and shortcomings <br></br>of ChatGPT and the User experience.
 </p>
                 
 </div>
@@ -40,7 +40,7 @@ const Videos = ()=>{
 
 
                 <div><iframe src="https://www.loom.com/embed/785e76518a494715a08761c75c05fb8f?sid=8a19a939-8a75-479f-9b82-36a43b386aac" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen className="video"></iframe>
-                <p className="text"><b>Episode 5:</b> The use case and impact of chatGPT</p>
+                <p className="text"><b>Episode 5:</b> The use case and impact of ChatGPT</p>
                
 </div>
          
