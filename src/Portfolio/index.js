eb676059117657208a76b99f85026b28be9cb15d
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-07-13 00:00:46 +0300

    made new changes

---
+++ src/Portfolio/index.js
@@ -0,0 +1,16 @@
+// import Gitahi from '../Gitahi';
+
+
+
+// function PortFolio(){
+//     return(
+//         <div className='portfolios'>
+//     <Gitahi/>
+   
+//         </div>
+
+//     )
+    
+
+// }
+// export default PortFolio;
